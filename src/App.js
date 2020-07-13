a3cdf4b68e5c3eafca2bc83b320c8338a4f24229
yujiniP <yuggi1004@nate.com>
2020-07-14 00:36:30 +0900

    200714  >> map,PropTypes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Bangtan({fav}) {
-  console.log(fav);
-  return<h3>L love {fav}!!!</h3>
+function Bangtan({ name, picture, rating}) {
+  return (
+    <div>
+      <h3>L love {name}!!!</h3>
+      <h4>{rating} / 7</h4>
+      <img src={picture} alt={name}  />
+    </div>
+  );
 }
+
+Bangtan.prototype= {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+const bts = [
+  {
+    id:"1",
+    name:"jimin",
+    image: "https://pbs.twimg.com/media/Ecz-SzmXYAghDYz?format=jpg&name=small",
+    rating: 6
+  },
+  {
+    id: "2",
+    name: "jk",
+    image: "https://pbs.twimg.com/media/Ecz-TRuXsAIBCD9?format=jpg&name=small",
+    rating: 5
+  },
+  {
+    id: "3",
+    name: "탄이들",
+    image: "https://pbs.twimg.com/media/Ecz65BnU8AAaBNc?format=jpg&name=small",
+    rating: 4
+  }
+]
 
 function App() {
   return (
     <div>
-      <Bangtan 
-      fav="bts"  
-      member ="jimin"
-      />
+      {bts.map(member => (
+        <Bangtan key={member.id} name={member.name} picture={member.image} rating={member.rating} />
+      ))}
     </div>
   );
 }
