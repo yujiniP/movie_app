6d26f2b73ba1656846e4936caf5d622d23f854e5
yujiniP <yuggi1004@nate.com>
2020-07-14 02:23:29 +0900

    200714 >> state

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-function Bangtan({ name, picture, rating}) {
-  return (
-    <div>
-      <h3>L love {name}!!!</h3>
-      <h4>{rating} / 7</h4>
-      <img src={picture} alt={name}  />
-    </div>
-  );
-}
-
-Bangtan.prototype= {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-const bts = [
-  {
-    id:"1",
-    name:"jimin",
-    image: "https://pbs.twimg.com/media/Ecz-SzmXYAghDYz?format=jpg&name=small",
-    rating: 6
-  },
-  {
-    id: "2",
-    name: "jk",
-    image: "https://pbs.twimg.com/media/Ecz-TRuXsAIBCD9?format=jpg&name=small",
-    rating: 5
-  },
-  {
-    id: "3",
-    name: "탄이들",
-    image: "https://pbs.twimg.com/media/Ecz65BnU8AAaBNc?format=jpg&name=small",
-    rating: 4
+class App extends React.Component {
+  state = {
+    isLoading:true
+  };
+  componentDidMount() {
+    setTimeout( () => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
-]
-
-function App() {
-  return (
-    <div>
-      {bts.map(member => (
-        <Bangtan key={member.id} name={member.name} picture={member.image} rating={member.rating} />
-      ))}
-    </div>
-  );
+  render() {
+    const {isLoading} = this.state;
+    return (
+      <div>
+        {isLoading ? "Loadgin" : "we are Loading"}
+      </div>
+    );
+  }
 }
 
 export default App;
